3c7b16e29412715cb2fd373ce5aa408f4b452bbc
Bisix21 <119133287+Bisix21@users.noreply.github.com>
2023-10-15 07:30:28 -0700

    add cron schedule

---
+++ migrations/Version20231015122645.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231015122645 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE deposit CHANGE date_open date_open DATE NOT NULL, CHANGE date_close date_close DATE NOT NULL, CHANGE status status VARCHAR(25) DEFAULT \'0\' NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE deposit CHANGE date_open date_open DATETIME NOT NULL, CHANGE date_close date_close DATETIME NOT NULL, CHANGE status status VARCHAR(25) DEFAULT \'OPEN\' NOT NULL');
+    }
+}
