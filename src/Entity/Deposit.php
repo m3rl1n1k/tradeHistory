480ac1794f21ffa045e2ffe919341e2425b89403
Bisix21 <119133287+Bisix21@users.noreply.github.com>
2023-11-26 05:14:44 -0800

    commit

---
+++ src/Entity/Deposit.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DepositRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DepositRepository::class)]
+class Deposit
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?float $amount = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateOpen = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateClose = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAmount(): ?float
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(float $amount): static
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getDateOpen(): ?\DateTimeInterface
+    {
+        return $this->dateOpen;
+    }
+
+    public function setDateOpen(\DateTimeInterface $dateOpen): static
+    {
+        $this->dateOpen = $dateOpen;
+
+        return $this;
+    }
+
+    public function getDateClose(): ?\DateTimeInterface
+    {
+        return $this->dateClose;
+    }
+
+    public function setDateClose(\DateTimeInterface $dateClose): static
+    {
+        $this->dateClose = $dateClose;
+
+        return $this;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUserId(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
