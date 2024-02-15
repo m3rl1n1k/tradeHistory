864e25b0f1215c436aafcae5d64ce4a65bfeeaa8
Serhii <bisix21@gmail.com>
2024-02-15 19:10:21 +0100

    fix shit after rebase

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,15 @@
 import './bootstrap.js';
+import './lib/bootstrap/assets/dist/js/bootstrap.bundle.min.js'
+import './lib/bootstrap/assets/js/color-modes'
+import './lib/bootstrap/assets/dist/css/bootstrap.min.css'
 /*
  * Welcome to your app's main JavaScript file!
  *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
  */
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+import './lib/bootstrap/dashboard.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
