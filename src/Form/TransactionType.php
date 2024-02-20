cf634fdd2e3b31ae18808dd805dbbc0d381acbfa
Serhii <bisix21@gmail.com>
2024-02-20 11:18:28 +0100

    fix ChartService.php access

--- src/Form/TransactionType.php
+++ src/Form/TransactionType.php
@@ -20,47 +20,48 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class TransactionType extends AbstractType
 {
-    public function __construct(protected Security $security,
-    protected  UrlGeneratorInterface $urlGenerator)
-    {
-    }
-
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('amount', MoneyType::class, [
-                'currency' => 'CP'
-            ])->add('category', EntityType::class, [
-                'class' => Category::class,
-                'required' => false,
-                'choice_label' => function (Category $category): string {
-                    return $category->getName();
-                }
-            ])
-            ->add('type', ChoiceType::class,
-                [
-                    'choices' => [
-                        "Expense" => TransactionEnum::EXPENSE,
-                        "Income" => TransactionEnum::INCOME,
-//                        "Transaction" => TransactionEnum::TRANSACTION,
-                    ]
-                ])
-            ->add('date', DateType::class,[
-                'data' => new DateTime(),
-            ])
-            ->add('description', TextareaType::class, [
-                'required' => false,
-                'attr' => [
-                    'max' => 255,
-                ]
-            ]);
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Transaction::class,
-			'category' => []
-        ]);
-    }
+	public function __construct(protected Security              $security,
+								protected UrlGeneratorInterface $urlGenerator)
+	{
+	}
+	
+	public function buildForm(FormBuilderInterface $builder, array $options): void
+	{
+		$category = $options['category'];
+		$builder
+			->add('amount', MoneyType::class, [
+				'currency' => 'CP'
+			])->add('category', ChoiceType::class, [
+				'choices' => $category,
+				'required' => false,
+				'choice_label' => function (Category $category): string {
+					return $category->getName();
+				}
+			])
+			->add('type', ChoiceType::class,
+				[
+					'choices' => [
+						"Expense" => TransactionEnum::EXPENSE,
+						"Income" => TransactionEnum::INCOME,
+						"Transaction" => TransactionEnum::TRANSACTION,
+					]
+				])
+			->add('date', DateType::class, [
+				'data' => new DateTime(),
+			])
+			->add('description', TextareaType::class, [
+				'required' => false,
+				'attr' => [
+					'max' => 255,
+				]
+			]);
+	}
+	
+	public function configureOptions(OptionsResolver $resolver): void
+	{
+		$resolver->setDefaults([
+			'data_class' => Transaction::class,
+			'category' => null
+		]);
+	}
 }
