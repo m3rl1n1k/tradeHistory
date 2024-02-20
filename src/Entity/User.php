f9b6b098f15c3523ce491e21ec47334be5d8250a
Serhii <bisix21@gmail.com>
2024-02-20 12:54:41 +0100

    wallet entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Api\Entity\ApiToken;
 use App\Repository\UserRepository;
+use App\Wallet\Entity\Wallet;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -16,49 +17,53 @@ use Symfony\Component\Security\Core\User\UserInterface;
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
 	#[ORM\Id]
-	#[ORM\GeneratedValue]
-	#[ORM\Column]
-	private ?int $id = null;
+               	#[ORM\GeneratedValue]
+               	#[ORM\Column]
+               	private ?int $id = null;
 	
 	#[ORM\Column(length: 180, unique: true)]
-	private ?string $email = null;
+               	private ?string $email = null;
 	
 	#[ORM\Column]
-	private array $roles = [];
+               	private array $roles = [];
 	
 	/**
 	 * @var string The hashed password
 	 */
 	#[ORM\Column]
-	private ?string $password = null;
+               	private ?string $password = null;
 	
 	#[ORM\Column(nullable: true)]
-	private ?float $amount = null;
+               	private ?float $amount = null;
 	
 	#[ORM\OneToMany(mappedBy: 'user', targetEntity: ApiToken::class, orphanRemoval: true)]
-	private Collection $apiTokens;
+               	private Collection $apiTokens;
+
+    #[ORM\OneToMany(mappedBy: 'user', targetEntity: Wallet::class, orphanRemoval: true)]
+    private Collection $wallets;
 	
 	public function __construct()
-	{
-		$this->apiTokens = new ArrayCollection();
-	}
+               	{
+               		$this->apiTokens = new ArrayCollection();
+                 $this->wallets = new ArrayCollection();
+               	}
 	
 	public function getId(): ?int
-	{
-		return $this->id;
-	}
+               	{
+               		return $this->id;
+               	}
 	
 	public function getEmail(): ?string
-	{
-		return $this->email;
-	}
+               	{
+               		return $this->email;
+               	}
 	
 	public function setEmail(string $email): static
-	{
-		$this->email = $email;
-		
-		return $this;
-	}
+               	{
+               		$this->email = $email;
+               		
+               		return $this;
+               	}
 	
 	/**
 	 * A visual identifier that represents this user.
@@ -66,107 +71,137 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 	 * @see UserInterface
 	 */
 	public function getUserIdentifier(): string
-	{
-		return (string)$this->email;
-	}
+               	{
+               		return (string)$this->email;
+               	}
 	
 	public function getUserId(): int
-	{
-		return (int)$this->getId();
-	}
+               	{
+               		return (int)$this->getId();
+               	}
 	
 	/**
 	 * @see UserInterface
 	 */
 	public function getRoles(): array
-	{
-		$roles = $this->roles;
-		// guarantee every user at least has ROLE_USER
-		$roles[] = 'ROLE_USER';
-		
-		return array_unique($roles);
-	}
+               	{
+               		$roles = $this->roles;
+               		// guarantee every user at least has ROLE_USER
+               		$roles[] = 'ROLE_USER';
+               		
+               		return array_unique($roles);
+               	}
 	
 	public function setRoles(array $roles): static
-	{
-		$this->roles = $roles;
-		
-		return $this;
-	}
+               	{
+               		$this->roles = $roles;
+               		
+               		return $this;
+               	}
 	
 	/**
 	 * @see PasswordAuthenticatedUserInterface
 	 */
 	public function getPassword(): string
-	{
-		return $this->password;
-	}
+               	{
+               		return $this->password;
+               	}
 	
 	public function setPassword(string $password): static
-	{
-		$this->password = $password;
-		
-		return $this;
-	}
+               	{
+               		$this->password = $password;
+               		
+               		return $this;
+               	}
 	
 	/**
 	 * @see UserInterface
 	 */
 	public function eraseCredentials(): void
-	{
-		// If you store any temporary, sensitive data on the user, clear it here
-		// $this->plainPassword = null;
-	}
+               	{
+               		// If you store any temporary, sensitive data on the user, clear it here
+               		// $this->plainPassword = null;
+               	}
 	
 	public function getAmount(): ?float
-	{
-		return $this->amount;
-	}
+               	{
+               		return $this->amount;
+               	}
 	
 	public function setAmount(?float $amount): static
-	{
-		$this->amount = $amount;
-		
-		return $this;
-	}
+               	{
+               		$this->amount = $amount;
+               		
+               		return $this;
+               	}
 	
 	public function incrementAmount(float $amount): float
-	{
-		return $this->getAmount() + $amount;
-	}
+               	{
+               		return $this->getAmount() + $amount;
+               	}
 	
 	public function decrementAmount(float $amount): float
-	{
-		return $this->getAmount() - $amount;
-	}
+               	{
+               		return $this->getAmount() - $amount;
+               	}
 	
 	/**
 	 * @return Collection<int, ApiToken>
 	 */
 	public function getApiTokens(): Collection
-	{
-		return $this->apiTokens;
-	}
+               	{
+               		return $this->apiTokens;
+               	}
 	
 	public function addApiToken(ApiToken $apiToken): static
-	{
-		if (!$this->apiTokens->contains($apiToken)) {
-			$this->apiTokens->add($apiToken);
-			$apiToken->setUser($this);
-		}
-		
-		return $this;
-	}
+               	{
+               		if (!$this->apiTokens->contains($apiToken)) {
+               			$this->apiTokens->add($apiToken);
+               			$apiToken->setUser($this);
+               		}
+               		
+               		return $this;
+               	}
 	
 	public function removeApiToken(ApiToken $apiToken): static
-	{
-		if ($this->apiTokens->removeElement($apiToken)) {
-			// set the owning side to null (unless already changed)
-			if ($apiToken->getUser() === $this) {
-				$apiToken->setUser(null);
-			}
-		}
-		
-		return $this;
-	}
+               	{
+               		if ($this->apiTokens->removeElement($apiToken)) {
+               			// set the owning side to null (unless already changed)
+               			if ($apiToken->getUser() === $this) {
+               				$apiToken->setUser(null);
+               			}
+               		}
+               		
+               		return $this;
+               	}
+
+    /**
+     * @return Collection<int, Wallet>
+     */
+    public function getWallets(): Collection
+    {
+        return $this->wallets;
+    }
+
+    public function addWallet(Wallet $wallet): static
+    {
+        if (!$this->wallets->contains($wallet)) {
+            $this->wallets->add($wallet);
+            $wallet->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeWallet(Wallet $wallet): static
+    {
+        if ($this->wallets->removeElement($wallet)) {
+            // set the owning side to null (unless already changed)
+            if ($wallet->getUser() === $this) {
+                $wallet->setUser(null);
+            }
+        }
+
+        return $this;
+    }
 }
