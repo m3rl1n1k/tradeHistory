0a5fc809b81384ca2308c6d1a9932d1f44de4b4a
Serhii <bisix21@gmail.com>
2024-02-20 01:56:09 +0100

    fix category access

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,16 @@
 import './bootstrap.js';
 import './lib/bootstrap/assets/dist/js/bootstrap.bundle.min.js'
-import './lib/bootstrap/assets/js/color-modes'
-import './lib/bootstrap/assets/dist/css/bootstrap.min.css'
+import './lib/bootstrap/assets/js/color-modes.js'
+
 /*
  * Welcome to your app's main JavaScript file!
  *
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 import './styles/docsearch.css';
 import './lib/bootstrap/dashboard.css';
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
+import './lib/bootstrap/assets/dist/css/bootstrap.min.css'
