53c5b77bf996fecd1ae8cc2d87cd9a167fb0ed31
Serhii <bisix21@gmail.com>
2024-02-25 23:10:31 +0100

    close access from another accounts to all wallets

---
+++ src/Controller/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use App\Security\Access;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/user')]
+class UserController extends AbstractController
+{
+	public function __construct(
+		protected Access $access,
+		protected UserRepository $userRepository)
+	{
+	}
+	
+	#[Route('/{id}', name: 'app_user_index', methods: ['GET', 'POST'])]
+	public function index(int $id, Request $request, User $user, EntityManagerInterface $entityManager):Response
+	{
+		$form = $this->createForm(UserType::class, $user);
+		$form->handleRequest($request);
+		
+		if ($form->isSubmitted() && $form->isValid()) {
+			$entityManager->flush();
+			
+			return $this->redirectToRoute('app_user_index', ['id'=>$id], Response::HTTP_SEE_OTHER);
+		}
+		
+		return $this->render('user/index.html.twig', [
+			'user' => $user,
+			'form' => $form,
+		]);
+	}
+}
