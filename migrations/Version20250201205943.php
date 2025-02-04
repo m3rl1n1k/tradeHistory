199122e2afcc3e3bdb648f0733bf87d3c69c3dcc
m3rl1n1k <m3rl1n1k@gmail.com>
2025-02-04 15:41:57 +0100

    Code cleaning

--- migrations/Version20250201205943.php
+++ migrations/Version20250201205943.php
@@ -20,7 +20,6 @@ final class Version20250201205943 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user_setting DROP color_picker_in_form');
         $this->addSql('ALTER TABLE wallet ADD is_main BOOLEAN DEFAULT false');
     }
 
@@ -28,7 +27,6 @@ final class Version20250201205943 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE wallet DROP is_main');
-        $this->addSql('ALTER TABLE user_setting ADD color_picker_in_form BOOLEAN DEFAULT NULL');
+        $this->addSql('ALTER TABLE wallet DROP is_main');;
     }
 }
