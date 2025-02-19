a3d689bbd04dff1a417ccc75c0cc2bac2ffc9b90
m3rl1n1k <m3rl1n1k@gmail.com>
2025-02-19 23:46:42 +0100

    use tilewind without webcore

--- assets/app.js
+++ assets/app.js
@@ -5,16 +5,9 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 // any CSS you import will output into a single css file (app.css in this case)
-import {startStimulusApp} from '@symfony/stimulus-bridge';
 import './styles/app.css';
 
 
-import './js/user_blockInputs'
-import './js/user_settings'
-import './js/search'
-
-export const app = startStimulusApp(require.context(
-    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
-    true,
-    /\.([jt])sx?$/
-));
+import './js/user_blockInputs.js'
+import './js/user_settings.js'
+import './js/search.js'
--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,5 @@ return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
 ];
