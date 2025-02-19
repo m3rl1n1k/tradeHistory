174e83986d00a76e81bf0518b71b2684f8bf0509
root <root@tradehistory.online>
2025-02-19 01:25:18 +0100

    fix styles

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,8 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('user', './assets/js/user_blockInputs.js')
+    .addEntry('settings', './assets/js/user_settings.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .addEntry('user', './assets/js/user_blockInputs.js')
