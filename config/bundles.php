7ece1af95f219d93b4333c745c6acc61c0290807
root <root@tradehistory.online>
2025-02-21 23:48:57 +0100

    Committer: root <root@tradehistory.online>  Changes to be committed: 	modified:   .gitignore 	modified:   composer.json 	modified:   composer.lock 	modified:   config/bundles.php 	deleted:    package-lock.json 	modified:   package.json 	modified:   public/build/manifest.json 	deleted:    tailwind.config.js 	deleted:    webpack.config.js

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
