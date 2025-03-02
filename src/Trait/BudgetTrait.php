0719442162c62e93e5542c060734ff6a9402921b
m3rl1n1k <m3rl1n1k@gmail.com>
2025-03-02 20:30:41 +0100

    * fix access to categories

---
+++ src/Trait/BudgetTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Trait;
+
+use App\Entity\Budget;
+use App\Form\BudgetType;
+use DateTime;
+use Symfony\Component\Form\FormInterface;
+
+trait BudgetTrait
+{
+    protected function getForm(Budget $budget): FormInterface
+    {
+        return $this->createForm(BudgetType::class, $budget, [
+            'category' => array_map(function ($category) {
+                return $category['categories'];
+            }, $this->parentCategoryRepository->getMainAndSubCategories()),
+            'budget' => $budget,
+            'date' => new DateTime()
+        ]);
+    }
+}
