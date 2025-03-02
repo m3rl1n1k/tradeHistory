46376e83057148d2bc4f7d33d565f491b75121fc
m3rl1n1k <m3rl1n1k@gmail.com>
2025-03-02 21:23:55 +0100

    * monthPicker.js

--- public/assets/js/monthPicker.js
+++ public/assets/js/monthPicker.js
@@ -1,8 +1,9 @@
 let month_picker = document.getElementById('budget_month')
 
-document.addEventListener('DOMContentLoaded', function (){
-    if (month_picker !== null){
-        month_picker.value = ''
-        month_picker.type = 'month'
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    month_picker.addEventListener('focus', function () {
+        if (month_picker !== null) {
+            month_picker.type = 'month'
+        }
+    })
 })
